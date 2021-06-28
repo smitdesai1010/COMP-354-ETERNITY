89fdb7a97c87cd44383e650156bf977dc462a036
Gabriel Desroches <44992765+gabriel-desroches@users.noreply.github.com>
2021-06-28 12:07:14 -0400

    Create MeanAbsoluteDeviation.java
    Need to do better input validation but won't crash for now

---
+++ MeanAbsoluteDeviation.java
@@ -0,0 +1,46 @@
+//We can change the naming conventions later
+public class MeanAbsoluteDeviation {
+
+    public static void main(String[] args) {
+        // For Testing, this is how method is used
+        double[] data = { 1.0, 2.0, 3.0 };
+        System.out.println(MAD(data));
+    }
+
+    // Mean Abosulute Deviation
+    public static double MAD(double[] dataSet) {
+        // input validation
+        if (dataSet.length == 0) {
+            System.out.println("\nError in data provided to MAD function\n");
+            return 0.0;
+        }
+
+        double dataMean = mean(dataSet);
+        double distancesFromMean[] = new double[dataSet.length];
+
+        // Calulating the absolute distance of each point from the mean
+        for (int i = 0; i < dataSet.length; i++) {
+            distancesFromMean[i] = dataSet[i] - dataMean;
+            // absolute value
+            if (distancesFromMean[i] < 0)
+                distancesFromMean[i] *= -1;
+        }
+
+        // return average of all the distances
+        return mean(distancesFromMean);
+    }
+
+    //Calculate mean of double list, can make public for reuse later
+    private static double mean(double[] list) {
+
+        if (list.length == 0)
+            return 0;
+
+        double sum = 0;
+        for (double n : list) {
+            sum += n;
+        }
+        return sum / list.length;
+    }
+
+}
