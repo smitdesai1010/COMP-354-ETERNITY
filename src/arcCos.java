33060ad54d5889ff29cd5bcc2ed6229c9e23c144
Trevor DSilva <46114769+detbrownbear@users.noreply.github.com>
2021-08-06 19:31:07 -0400

    cleaned up code

--- src/arcCos.java
+++ src/arcCos.java
@@ -1,159 +1,145 @@
-package src; 
-
-
 public class arcCos{
 
     static double pi =  3.14159265358979;
+    static double EPS = 0.0000000000001;
 
 
-
-//string parser used to evaluate strings into doubles
-// public static double eval(final String str) {
-//         return new Object() {
-//             int pos = -1, ch;
-    
-//             void nextChar() {
-//                 ch = (++pos < str.length()) ? str.charAt(pos) : -1;
-//             }
-    
-//             boolean eat(int charToEat) {
-//                 while (ch == ' ') nextChar();
-//                 if (ch == charToEat) {
-//                     nextChar();
-//                     return true;
-//                 }
-//                 return false;
-//             }
-    
-//             double parse() {
-//                 nextChar();
-//                 double x = parseExpression();
-//                 if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
-//                 return x;
-//             }
-    
-//             // Grammar:
-//             // expression = term | expression `+` term | expression `-` term
-//             // term = factor | term `*` factor | term `/` factor
-//             // factor = `+` factor | `-` factor | `(` expression `)`
-//             //        | number | functionName factor | factor `^` factor
-    
-//             double parseExpression() {
-//                 double x = parseTerm();
-//                 for (;;) {
-//                     if      (eat('+')) x += parseTerm(); // addition
-//                     else if (eat('-')) x -= parseTerm(); // subtraction
-//                     else return x;
-//                 }
-//             }
-    
-//             double parseTerm() {
-//                 double x = parseFactor();
-//                 for (;;) {
-//                     if      (eat('*')) x *= parseFactor(); // multiplication
-//                     else if (eat('/')) x /= parseFactor(); // division
-//                     else return x;
-//                 }
-//             }
-    
-//             double parseFactor() {
-//                 if (eat('+')) return parseFactor(); // unary plus
-//                 if (eat('-')) return -parseFactor(); // unary minus
-    
-//                 double x;
-//                 int startPos = this.pos;
-//                 if (eat('(')) { // parentheses
-//                     x = parseExpression();
-//                     eat(')');
-//                 } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
-//                     while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
-//                     x = Double.parseDouble(str.substring(startPos, this.pos));
-//                 } else if (ch >= 'a' && ch <= 'z') { // functions
-//                     while (ch >= 'a' && ch <= 'z') nextChar();
-//                     String func = str.substring(startPos, this.pos);
-//                     x = parseFactor();
-//                     if (func.equals("sqrt")) x = mySqrt(x);
-//                     // else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
-//                     // else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
-//                     // else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
-//                     else throw new RuntimeException("Unknown function: " + func);
-//                 } else {
-//                     throw new RuntimeException("Unexpected: " + (char)ch);
-//                 }
-    
-//                 if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
-    
-//                 return x;
-//             }
-//         }.parse();
-//     }
 
 
 //simple function to convert radians to degrees 
-public static double radToDeg(double rad){
-   double retDeg = rad*180/pi;
-    
-    return retDeg;
-}
-
-
-//implementation of a root finder using the Newton's method, 200 iterations 
-    public static double mySqrt(double x) {
-        if (x == 0)
-            return 0;
-
-        double n = 1; 
-        double cnt=1; 
-                while(true) {
-                    if(cnt == 50) {
-                        return  n; 
-            }
-            n = n - (( (n*n)- x) /  (2*n)); 
-            cnt+=1;
-        }
+    public static double radToDeg(double rad){
+    double retDeg = rad*180/pi;
         
+        return retDeg;
     }
 
- //A fast arccosine implementation, accurate to about 0.5 degrees, can be based on the observation that for x in [0,1], acos(x) ≈ √(2*(1-x)). 
- //An additional scale factor improves accuracy near zero. The optimal factor can be found by a simple binary search. Negative arguments are handled according to acos (-x) = π - acos (x).   
-   public static double acos(double x) throws IllegalArgumentException{
-    double C = 0.10501094;
-    double r,s,t,u;
-    
-    try {
-        
 
-        else if (x == -.5){
-            return 2*pi/3;
+    //implementation of a root finder using the Newton's method, 50 iterations 
+        public static double mySqrt(double x) {
+            if (x == 0)
+                return 0;
+
+            double n = 1; 
+            double cnt=1; 
+                    while(true) {
+                        if(cnt == 50) {
+                            return  n; 
+                }
+                n = n - (( (n*n)- x) /  (2*n)); 
+                cnt+=1;
+            }
+            
+        }
+
+
+
+
+
+//revised function with base cases because of approximation. Uses equations from Doerfler's Dead Reckoning
+    public static double acos(double x){
+        if (x == -.5){
+            return 120;
         }
         else if (x == -1){
-            return pi;
+            return 180;
         }
         else if (x == 1){
             return 0;    
         }
         else if (x == 0){
-            return pi/2;
+            return 90;
         }
         else if (x == .5){
-            return pi/3;
+            return 60;
         }
 
-        else if (x < 0)
-            t = -x;
-        else 
-            t = x; 
-
-        u = 1.0f - t;
-        s = mySqrt(u + u);
-        r = C * u * s + s;
-        if ( x < 0 ) 
-            r = pi - r;
-
-        return r; 
+       double retNum;
+       double num, denum, temp;
+       temp = x * x;
+       num = 1.0 - temp;
+       temp = num;
+       
+       temp = exponentiation(num, .5);
+       num = 3.0 * temp;
+       denum = x + 2;
+       retNum = num / denum;
+       retNum *= 401/7;
+    return retNum;
         
+    }
 
-        
-}
+    static public double myPow(double x, int n) {
+
+        int ret = n;
+            if(n==0) {
+            return 1;
+            }
+            if(n<0) {
+            ret=(int)myAbs(n); //if n=-1, then make it n=1 and return reciprocal of result
+            double result= myPow(x, ret-1)*x;
+            return (1.0/result);
+            }
+            double k= myPow(x, ret/2);
+            double powNumber=k*k;
+            if(ret%2==1) { //if n is odd
+            powNumber=powNumber *x;
+            }
+            return powNumber;
+            }
+
+
+
+
+    
+
+
+    public static double myAbs(double x){
+        double ret = x;
+        if (ret < 0){
+            ret = -ret;    
+        }
+        return ret;
+    }
+
+    public static double exponentiation(double base, double exp){
+    if (exp % 1 == 0 ){
+    return myPow(base, (int)exp);
+    }
+
+    if(exp == 0)
+        return 1.0;
+    else if (exp < 0){
+        return 1.0 / exponentiation(base, -exp);
+    }
+    
+    else if(exp >= 1){
+        double temp = exponentiation(base, exp / 2);
+        return temp * temp;
+    } 
+    else{
+        double low = 0;
+        double high = 1.0;
+
+        double sqr = mySqrt(base);
+        double acc = sqr;    
+        double mid = high / 2.0;
+
+        while(myAbs(mid - exp) > EPS){
+        sqr = mySqrt(sqr);
+
+        if (mid <= exp) {
+            low = mid;
+            acc *= sqr;
+        } else{
+            high = mid;
+            acc *= (1.0/sqr);
+        }
+
+        mid = (low + high) / 2;
+        }
+
+        return acc;
+    }
+    }
 
 }
